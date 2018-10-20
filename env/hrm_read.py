31e73f9bf549d9e74d2d2d49964ac11a0085d1d8
Haeryn Kim <hk181@duke.edu>
2018-10-20 17:39:30 -0400

    Added function that reads maximum and minimum voltage in the voltage array.

--- env/hrm_read.py
+++ env/hrm_read.py
@@ -7,3 +7,15 @@ def read_data(filename):
     time = data[:, 0]
     voltage = data[:, 1]
     return time, voltage
+
+def voltage_extreme(V):
+    """ This function returns the max and min voltages found from a voltage array in a tuple form.
+        :param V:
+    :return:
+
+
+    later: add test to see if voltage_extreme returns tuple. add exceptions to see if a list is passed as input.
+    """
+    maxV = max(V)
+    minV = min(V)
+    return tuple([minV,maxV])
