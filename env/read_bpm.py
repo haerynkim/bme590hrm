1da200a67817f4321eaa35105bcc35f3e3760d15
Haeryn Kim <hk181@duke.edu>
2018-10-23 15:49:16 -0400

    Get rid of input prompt and make start_time and end_time as arguments to the function that will average heart rate over that interval.

--- env/read_bpm.py
+++ env/read_bpm.py
@@ -1,26 +1,18 @@
 from hrm_read import read_data
 from find_peak import find_peak
 
-def read_bpm(filename, default_min=20):
+def read_bpm(filename, starttime=0, endtime=20):
     """ This function calculates the average heart rate over user-specified number of minutes.
 
     :param filename: .csv file
     :return:
     """
     time, voltage = read_data(filename)
-    try:
-        user_sec = input("Input number of seconds you want to average heart rate over (input integer value.)")
-    except SyntaxError:
-        user_sec = None
-    if user_sec is None:
-        user_sec = default_min
-    if type(user_sec) is not int:
-        raise TypeError("Inputs must be python integers.") #I want this later to try rounding up.
-        # How to default?
     #if user_min < 0 or user_min >
-    idx = time.index(user_sec)
-    time = time[:idx]
-    voltage = voltage[:idx]
+    startidx = time.index(starttime)
+    endidx = time.index(endtime)
+    time = time[startidx:endidx]
+    voltage = voltage[startidx:endidx]
     peakidx = find_peak(voltage, thres=0.8, min_dist=0.1)
     peaktimes = time[peakidx]
     rr_int = [t-s for s, t in zip(peaktimes, peaktimes[1:])]
