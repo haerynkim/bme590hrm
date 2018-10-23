b98991b83e842890dcf6b3bebf8695f6b8dc45c8
Haeryn Kim <hk181@duke.edu>
2018-10-23 14:43:53 -0400

    Added function that creates a dictionary that will store 'metrics'.

---
+++ env/dictionary.py
@@ -0,0 +1,8 @@
+def create_dictionary():
+    metrics = {
+        "mean_hr_bpm": 0,
+        "voltage_extremes": 0,
+        "duration": 0,
+        "num_beats": 0,
+        "beats": 0}
+    return metrics
