cf8bca22f2aa3da61b35db3dd19c791d10466b3a
Haeryn Kim <hk181@duke.edu>
2018-10-23 22:52:19 -0400

    Add function that will write the dictionary into a json file. Also add gitignore file to avoid committing unnecessary changes.

---
+++ env/write_json.py
@@ -0,0 +1,13 @@
+import json
+
+def write_json(filename, dict):
+    """
+
+    :return:
+    """
+    #check if filename = string
+    filename = filename.replace('csv','json')
+    idx = filename.rfind('/')+1
+    jsonname = filename[idx:]
+    with open(jsonname, 'w') as file:
+        json.dump(dict, file)
