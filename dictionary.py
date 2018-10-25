0063b6d5f701f4ada547339c15184ded8572eb65
Haeryn Kim <hk181@duke.edu>
2018-10-25 16:40:26 -0400

    Fix minor syntax errors regarding strings on dictionary testing function.

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,6 @@
 def add_to_dictionary(dict, key, val):
     #test if key is string
-    if type(key) != 'str':
+    if type(key) != str:
         raise TypeError("Key is not a string")
     dict[key] = val
     return dict
--- test_add_to_dictionary.py
+++ test_add_to_dictionary.py
@@ -1,13 +1,13 @@
 import pytest
 from dictionary import add_to_dictionary
 
-goodgoods = {"Burger": 8.7,
-             "Ramen": 5.8
+goodgoods = {'Burger': 8.7,
+             'Ramen': 5.8
 }
 
 def test_add_to_dictionary():
-    dictionary = add_to_dictionary(goodgoods, "Sushi", 9.0)
-    assert goodgoods["Sushi"] == 9.0
+    dictionary = add_to_dictionary(goodgoods, 'Sushi', 9.0)
+    assert goodgoods['Sushi'] == 9.0
 
 def test_excetion():
     with pytest.raises(TypeError):
