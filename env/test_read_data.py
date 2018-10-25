a56792d829cc68c13717e908de86367a7a7395c6
Haeryn Kim <hk181@duke.edu>
2018-10-25 01:31:01 -0400

    Replace pytest.mark.parametrize by pytest.fixture to import test data as a fixture to test the read_data function. Need help on this.

--- env/test_read_data.py
+++ env/test_read_data.py
@@ -1,12 +1,21 @@
 #This code tests my function read_data in the file hrm_read.py
+import pytest
 from hrm_read import read_data
 
+@pytest.fixture
+def file_loader():
+    """Loads data from locally available test data"""
 
-@pytest.mark.parametrize("candidate, expected1, expected2", [
-    (dummy file, val1, val2)
-])
+    def _loader(filename):
+        fileread = numpy.genfromtxt(filename, delimiter=',')
+    return fileread
 
-def test_read_data(candidate, expected1, expected2):
-    response1, response2 = read_data(candidate)
-    assert response1 == expected1
-    assert response2 == expected2
+#@pytest.mark.parametrize("candidate, expected1, expected2", [
+#    (dummy file, val1, val2)
+#])
+
+def test_read_data(candidate, expected, file_loader):
+    response1, response2 = read_data('env/ECGdata/test_data1.csv')
+    t, V = file_loader()[:,0], file_loader()[:,1]
+    assert response1 == t
+    assert response2 == V
