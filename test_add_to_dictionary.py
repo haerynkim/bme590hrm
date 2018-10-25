09f50d05c70190ee4445d20b77e9c8c90b866605
Haeryn Kim <hk181@duke.edu>
2018-10-25 16:34:49 -0400

    Add testing function for add_to_dictionary dictionary to test key-value pair addition and for correct input types.

---
+++ test_add_to_dictionary.py
@@ -0,0 +1,14 @@
+import pytest
+from dictionary import add_to_dictionary
+
+goodgoods = {"Burger": 8.7,
+             "Ramen": 5.8
+}
+
+def test_add_to_dictionary():
+    dictionary = add_to_dictionary(goodgoods, "Sushi", 9.0)
+    assert goodgoods["Sushi"] == 9.0
+
+def test_excetion():
+    with pytest.raises(TypeError):
+        add_to_dictionary(goodgoods, 9.8, 5.5)
