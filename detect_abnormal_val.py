e1557068b5c5501b992970889031a72864473544
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:31:00 -0400

    Edit docstring in detect_abnormal_val

--- detect_abnormal_val.py
+++ detect_abnormal_val.py
@@ -2,9 +2,9 @@ def detect_abnormal_val(V, abs_range=200):
     """This function will see if any value in numpy array V exceeds the range of a user-
     specified value (integer value, defaulted to 200).
 
-    :param V: numpy ndarray
+    :param V: numpy.ndarray
     :param abs_range: int
-    :return: ValueError if value abnormal
+    :return: ValueError (if value is abnormal)
     """
     for i in V:
         if abs(i) >= abs_range:
