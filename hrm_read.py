424d635fdb98a85ce94f7de593fd193dfb30b701
Haeryn Kim <hk181@duke.edu>
2018-10-25 22:54:35 -0400

    Change function to find min, max in numpy array not list

--- hrm_read.py
+++ hrm_read.py
@@ -1,35 +1,36 @@
 import numpy
 
-def read_data(filename):
-    """ This function reads a .csv file and extracts time and voltage arrays.
 
-    :param filename: .csv files
-    :return: lists
+def read_data(filename):
+    """ This function reads a .csv file and extracts time and voltage arrays. The 'numpy.genfromtxt' function is a
+    robust mechanism that internally screens for strings and missing values and leaves those rows out from the numpy
+    arrays that are returned.
+
+    :param filename: .csv file
+    :return: time: numpy.ndarray, voltage: numpy.ndarray
     """
     data = numpy.genfromtxt(filename, delimiter=',')
     time = data[:, 0]
     voltage = data[:, 1]
     return time, voltage
 
+
 def voltage_extremes(V):
-    """ This function returns the max and min voltages found from a voltage array in a tuple form.
+    """ This function returns the minimum and maximum voltages found from a voltage array in a tuple form.
 
-    :param V: list
-    :return: tuple
-
-    later: add test to see if voltage_extreme returns tuple. add exceptions to see if a list is passed as input.
+    :param V: numpy.ndarray
+    :return: tuple (minimum voltage, maximum voltage)
     """
-    maxV = max(V)
-    minV = min(V)
-    return (minV,maxV)
+    maxV = numpy.amax(V)
+    minV = numpy.amin(V)
+    return (minV, maxV)
+
 
 def duration(t):
     """ This function returns a float that represents the duration of the ECG strip.
 
-    :param t: list
+    :param t: numpy.ndarray
     :return: float
-
-    add test that checks if t is list
     """
     dur = t[-1] - t[0]
     return float(dur)
