489c7d07bce6e7c04eebe6fe7761ac64e80d1a97
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:32:54 -0400

    Edit docstring in find_peak

--- find_peak.py
+++ find_peak.py
@@ -1,13 +1,13 @@
 import numpy as np
 import peakutils
 
-def find_peak(corr, thres=0.8, min_dist=0.1):
+def find_peak(corr, thres=0.5, min_dist=0.1):
     """
-    This function finds the peaks of a given an array, given parameters like threshold (default 0.8) and minimum
+    This function finds the peaks of a given an array, given parameters like threshold (default 0.5) and minimum
     distance between peaks (default 0.1). It will return the indexes at which these peaks happened, and the number of
-    peaks detected.
+    peaks that were detected.
 
-    :param corr: list
+    :param corr: numpy.ndarray
     :param thres: float
     :param min_dist: float
     :return: beatidx: 'numpy.ndarray', num_beats: 'int'
