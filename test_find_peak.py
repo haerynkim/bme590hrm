6e08679e11066dbf841296e580bd3237609678db
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:47:40 -0400

    Add docstring to find_peak unit test

--- test_find_peak.py
+++ test_find_peak.py
@@ -1,6 +1,11 @@
 from find_peak import find_peak
+import numpy
+
 
 def test_find_peak():
-    b, nb = find_peak([0,0,0,0,5,0,7,8.9,3,1,1,2,0.5], thres=0.8, min_dist=0.1)
+    """
+    This test checks if the correct value and number of local maxima are returned.
+    """
+    b, nb = find_peak(numpy.array([0, 0, 0, 0, 5, 0, 7, 8.9, 3, 1, 1, 2, 0.5]), thres=0.8, min_dist=0.1)
     assert [7]
     assert nb == 1
