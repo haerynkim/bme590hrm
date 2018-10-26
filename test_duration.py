5278358d11233fe0aec086b0b868a185dc914b21
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:44:36 -0400

    Add docstring to duration unit test

--- test_duration.py
+++ test_duration.py
@@ -1,12 +1,17 @@
 from hrm_read import duration
 import pytest
+import numpy
+
 
 @pytest.mark.parametrize("candidate, expected", [
-    ([1, 2, 3, 4, 5, 6], 5.0),
-    ([3, 6, 12, 15, 18, 2, 9], 6.0),
-    ([7, 6, 212, 90, 8], 1.0)
+    (numpy.array([1, 2, 3, 4, 5, 6]), 5.0),
+    (numpy.array([3, 6, 12, 15, 18, 2, 9]), 6.0),
+    (numpy.array([7, 6, 212, 90, 8]), 1.0)
 ])
-
 def test_duration(candidate, expected):
+    """
+    This test checks if the function returns a float value that is the difference between the last and first element of
+    the numpy array.
+    """
     response = duration(candidate)
     assert response == expected
