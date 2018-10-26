15ece8d44b8570ee7c59e1572534915518364025
Haeryn Kim <hk181@duke.edu>
2018-10-25 22:22:40 -0400

    Fix incompatibility of variable types.

--- read_bpm.py
+++ read_bpm.py
@@ -1,3 +1,4 @@
+import numpy
 from hrm_read import read_data
 from find_peak import find_peak
 
@@ -5,16 +6,23 @@ def read_bpm(filename, starttime=0, endtime=20):
     """ This function calculates the average heart rate over user-specified number of minutes.
 
     :param filename: .csv file
-    :return:
+    :param starttime: int
+    :param endtime: int
+    :return: float
     """
     time, voltage = read_data(filename)
-    startidx = time.index(starttime)
-    endidx = time.index(endtime)
-    time = time[startidx:endidx]
-    voltage = voltage[startidx:endidx]
-    peakidx, numpeak = find_peak(voltage, thres=0.8, min_dist=0.1)
+    startidx = numpy.where(time==starttime)[0][0]
+    endidx = numpy.where(time==endtime)[0][0]
+    time = time[startidx:endidx+1]
+    voltage = voltage[startidx:endidx+1]
+    peakidx, numpeak = find_peak(voltage, thres=0.5, min_dist=0.1)
     peaktimes = time[peakidx]
     rr_int = [t-s for s, t in zip(peaktimes, peaktimes[1:])]
     rr_avg = sum(rr_int) / float(len(rr_int))
     mean_hr_bpm = 60/rr_avg
-    return mean_hr_bpm
+    return float(mean_hr_bpm)
+
+if __name__ == "__main__":
+    bpm = read_bpm('ECGdata/test_data5.csv', 0, 20)
+    print(bpm)
+    print(type(bpm))
