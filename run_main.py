7b243080248aba71b0b37268f093a15766d0cd16
Haeryn Kim <hk181@duke.edu>
2018-10-26 00:31:44 -0400

    Commit changes for error in detect_abnormal_val

--- run_main.py
+++ run_main.py
@@ -7,18 +7,18 @@ from find_peak import find_peak
 from read_bpm import read_bpm
 from dictionary import add_to_dictionary
 from write_json import write_json
-
-filename = 'ECGdata/test_data1.csv'
+from detect_abnormal_val import detect_abnormal_val
+import numpy
 
 def main(filename):
     time, voltage = read_data(filename)
     try:
-        detect_abnormal_val(voltage)
+        detect_abnormal_val(voltage, abs_range=200)
     except ValueError:
         sys.exit('Voltage out of this world. Consult doctor immediately.')
-    mean_hr_bpm = read_bpm(filename, starttime=0, endtime=20)
-    correlate = cross_corr(voltage, startpi = 0.5*numpy.pi, endpi = 1.5*numpy.pi, intrp=15)
-    beatidx, num_beats = find_peak(correlate, thres=0.8, min_dist=0.1)
+    mean_hr_bpm = read_bpm(time, voltage, starttime=0, endtime=20)
+    correlate = cross_corr(voltage, startpi=0.5 * numpy.pi, endpi=1.5 * numpy.pi, intrp=15)
+    beatidx, num_beats = find_peak(correlate, thres=0.5, min_dist=0.1)
     beats = time[beatidx]
     voltage_extremes = voltage_extremes(voltage)
     duration = duration(time)
@@ -28,9 +28,9 @@ def main(filename):
     metrics = add_to_dictionary(metrics, 'duration', duration)
     metrics = add_to_dictionary(metrics, 'num_beats', num_beats)
     metrics = add_to_dictionary(metrics, 'beats', beats)
-    #outfile = write_json(filename, metrics)
-    #return outfile
+    write_json(filename, metrics)
+
 
 if __name__ == '__main__':
+    filename = 'ECGdata/test_data31.csv'
     main(filename)
-
