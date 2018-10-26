29faf602a304969dfc84697b34b2f3bf0cb25ebd
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:42:42 -0400

    Add docstring to detect_abnormal_val docstring

--- test_detect_abnormal_val.py
+++ test_detect_abnormal_val.py
@@ -2,6 +2,11 @@ import pytest
 import numpy
 from detect_abnormal_val import detect_abnormal_val
 
+
 def test_detect_abnormal_val():
+    """
+    This test checks if a ValueError is raised if any value in the input numpy array is greater in magnitude than a
+    user-specified value.
+    """
     with pytest.raises(ValueError):
         detect_abnormal_val(numpy.array([-250, 100]), 200)
