d28c37fe8aeebac58c96f6ec1166a61847e03d93
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:30:11 -0400

    Edit docstring of cross_corr

--- cross_corr.py
+++ cross_corr.py
@@ -2,14 +2,14 @@ import numpy
 
 def cross_corr(vol, startpi = 0.5*numpy.pi, endpi = 1.5*numpy.pi, intrp=15):
     """This function will generate a sine wave given an array of intrp number of elements
-    from start to end point and cross-correlate the voltage array with this sine wave.
+    from startpi (start point) to endpi (end point) and cross-correlate the vol array with this sine wave.
     It returns a numpy array of correlation coefficients.
 
-    :param vol: array
+    :param vol: numpy.ndarray
     :param startpi: float, starting point of sine wave
     :param endpi: float, end point of sine wave
-    :param intrp: int total number of points interpolated between starting point and end point
-    :return:
+    :param intrp: int, total number of points interpolated between starting point and end point
+    :return: numpy.ndarray
     """
     t = numpy.linspace(startpi, endpi, intrp)
     qrs_filter = numpy.sin(t)
