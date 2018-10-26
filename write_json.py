b0d4741aa0dac2b18f51f082ec7eb437c1051ffb
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:55:36 -0400

    Edit docstring to write_json

--- write_json.py
+++ write_json.py
@@ -1,13 +1,16 @@
 import json
 
+
 def write_json(filename, dict):
     """
+    This function takes a string and retrieves the filename to which it will generate a new json file and populate with
+    a given dictionary.
 
-    :return:
+    :param filename: str
+    :param dict: dictionary
     """
-    #check if filename = string
-    filename = filename.replace('csv','json')
-    idx = filename.rfind('/')+1
+    filename = filename.replace('csv', 'json')
+    idx = filename.rfind('/') + 1
     jsonname = filename[idx:]
     with open(jsonname, 'w') as file:
         json.dump(dict, file)
