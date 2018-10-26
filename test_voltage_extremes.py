abc5c1edba1447403aa90a643d42a4a19f0e2424
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:50:09 -0400

    Add docstring to voltage_extreme unit test

--- test_voltage_extremes.py
+++ test_voltage_extremes.py
@@ -1,13 +1,16 @@
-#This code tests my function voltage_extreme in the file hrm_read.py
 from hrm_read import voltage_extremes
 import pytest
+import numpy
 
 @pytest.mark.parametrize("candidate, expected", [
-    ([1, 5, 100], (1,100)),
-    ([8, 9, 10, 11], (8,11)),
-    ([-100, 9, 7, 65, 1], (-100, 65))
+    (numpy.array([1, 5, 100]), (1,100)),
+    (numpy.array([8, 9, 10, 11]), (8,11)),
+    (numpy.array([-100, 9, 7, 65, 1]), (-100, 65))
 ])
 
 def test_voltage_extremes(candidate, expected):
+    """
+    This test checks if a tuple containing the correct minimum and maximum value of a numpy array is returned.
+    """
     response = voltage_extremes(candidate)
     assert response == expected
