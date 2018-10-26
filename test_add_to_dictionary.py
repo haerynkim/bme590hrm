fb37c89e45ebfbf19d0b5f55af9e6dc15d55476a
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:36:52 -0400

    Add docstring to dictionary unit tests

--- test_add_to_dictionary.py
+++ test_add_to_dictionary.py
@@ -3,12 +3,20 @@ from dictionary import add_to_dictionary
 
 goodgoods = {'Burger': 8.7,
              'Ramen': 5.8
-}
+             }
+
 
 def test_add_to_dictionary():
+    """
+    This test checks if the given key-value pair has been added to a dictionary.
+    """
     dictionary = add_to_dictionary(goodgoods, 'Sushi', 9.0)
     assert goodgoods['Sushi'] == 9.0
 
+
 def test_excetion():
+    """
+    This test checks if a TypeError is raised when the key is not a string.
+    """
     with pytest.raises(TypeError):
         add_to_dictionary(goodgoods, 9.8, 5.5)
