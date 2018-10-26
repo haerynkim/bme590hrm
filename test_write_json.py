829ef84eeb96ce6ee088d94ed703447bdf22dfa7
Haeryn Kim <hk181@duke.edu>
2018-10-25 23:51:37 -0400

    Edit docstring in write_json unit test

--- test_write_json.py
+++ test_write_json.py
@@ -2,21 +2,27 @@ import os
 import pytest
 from write_json import write_json
 
+
 @pytest.fixture
 def mktmpdir(tmpdir_factory):
-    """create temporary directory for unit tests
+    """ This creates a temporary directory for unit tests.
     
-    :param tmpdir_factory:
+    :param tmpdir_factory: directory
     :return: tmpdir
     """
     tmpdir = tmpdir_factory.mktemp('tmp')
     return tmpdir
 
+
 @pytest.fixture
 def pathfile(mktmpdir):
+    """
+    This creates a .csv file in the temporary directory, mkrmpdir, and returns the file's pathway.
+    """
     file = mktmpdir.join('/emptyfile.csv')
     path = file.strpath
     return path
+
 
 def test_write_json(pathfile, mktmpdir):
     """ This function tests if the write_json function generates a json file after being passed a
@@ -26,9 +32,9 @@ def test_write_json(pathfile, mktmpdir):
     :param mktmpdir: pytest fixture that points to temporary directory
     """
     sampledict = {
-                'This': 9,
-                'Is': 8,
-                'Hard': 7
+        'This': 9,
+        'Is': 8,
+        'Hard': 7
     }
     write_json(pathfile, sampledict)
     assert os.path.exists('emptyfile.json') == True
